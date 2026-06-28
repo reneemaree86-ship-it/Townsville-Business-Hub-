6f2797595f94a6da792da53701e824239fed7178
reneemaree86-ship-it <reneemaree86@gmail.com>
2026-06-28 20:17:28 +1000

    fix: flatten import paths in toggle-group.jsx

--- src/toggle-group.jsx
+++ src/toggle-group.jsx
@@ -2,8 +2,8 @@
 import * as React from "react"
 import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group"
 
-import { cn } from "@/lib/utils"
-import { toggleVariants } from "@/components/ui/toggle"
+import { cn } from "@/utils"
+import { toggleVariants } from "@/toggle"
 
 const ToggleGroupContext = React.createContext({
   size: "default",
